4df50a4b83a0109d3fe2fbde6004f0e8b7cfece9
Aaron Mathew <aaronmathew453@gmail.com>
2024-09-10 21:15:47 +0530

    first commit

---
+++ src/components/structure/Structure.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Structure = () => {
+  return (
+    <div className='h-screen w-screen bg-blue-400'>
+        <div></div>
+    </div>
+  )
+}
+
+export default Structure
