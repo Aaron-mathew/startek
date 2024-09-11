77f1701df610384e483de52857cf4b4ee4be5ac6
Aaron Mathew <aaronmathew453@gmail.com>
2024-09-11 23:57:01 +0530

    added logo

--- src/components/structure/Structure.jsx
+++ src/components/structure/Structure.jsx
@@ -7,6 +7,9 @@ const Structure = () => {
             <img className='absolute top-0 left-0 w-1/2' src="https://cdn.prod.website-files.com/65ef0d86eff3b9ba6b18aef9/65f2ff660acfee8ba5cd3b36_border-left-top.svg" alt="" />
             <img className='absolute top-0 right-0 w-1/2' src="https://cdn.prod.website-files.com/65ef0d86eff3b9ba6b18aef9/66603af083943342fe214cf9_border-right-top-new.svg" alt="" />
         </div>
+        <div className='fixed top-10 w-full'>
+            <img className='absolute -ml-[17vw] h-[8vh] top-0 w-1/2' src="https://cdn.prod.website-files.com/65ef0d86eff3b9ba6b18aef9/660303290d04849733a63177_startech%20full.svg" alt="" />
+        </div>
         <div className='fixed left-0 top-1/2 -translate-y-1/2'>
             <img className='w-24' src="https://cdn.prod.website-files.com/65ef0d86eff3b9ba6b18aef9/65f3025ed598b85f146d736d_border-left-center.svg" alt="" />
         </div>
@@ -34,35 +37,31 @@ const Structure = () => {
             </svg>        
         </div>
         <div className='fixed -right-48 top-1/2 -translate-y-1/2'>
-        <svg xmlns="http://www.w3.org/2000/svg" width="13%" height="13%" color='white' viewBox="0 0 37 152" fill="none">
-            <path d="M25.55 0.999802L10.9501 0.999801" stroke="currentColor"></path>
-            <path d="M25.55 9.3427L10.9501 9.3427" stroke="currentColor"></path>
-            <path d="M25.55 17.6855L10.9501 17.6855" stroke="currentColor"></path>
-            <path d="M25.55 26.0285L10.9501 26.0285" stroke="currentColor"></path>
-            <path d="M25.55 34.3712L10.9501 34.3712" stroke="currentColor"></path>
-            <path d="M25.55 42.714L10.9501 42.714" stroke="currentColor"></path>
-            <path d="M25.55 51.057L10.9501 51.057" stroke="currentColor"></path>
-            <path d="M25.55 59.3997L10.9501 59.3997" stroke="currentColor"></path>
-            <path d="M25.55 67.7425L10.9501 67.7425" stroke="currentColor"></path>
-            <path d="M36.5 76.0854L7.83205e-05 76.0853" stroke="currentColor"></path>
-            <path d="M25.55 84.4284L10.9501 84.4284" stroke="currentColor"></path>
-            <path d="M25.55 92.7713L10.9501 92.7713" stroke="currentColor"></path>
-            <path d="M25.55 101.114L10.9501 101.114" stroke="currentColor"></path>
-            <path d="M25.55 109.457L10.9501 109.457" stroke="currentColor"></path>
-            <path d="M25.55 117.8L10.9501 117.8" stroke="currentColor"></path>
-            <path d="M25.55 126.143L10.9501 126.143" stroke="currentColor"></path>
-            <path d="M25.55 134.485L10.9501 134.485" stroke="currentColor"></path>
-            <path d="M25.55 142.828L10.9501 142.828" stroke="currentColor"></path>
-            <path d="M25.55 151.171L10.9501 151.171" stroke="currentColor"></path>
-        </svg>       
+            <svg xmlns="http://www.w3.org/2000/svg" width="13%" height="13%" color='white' viewBox="0 0 37 152" fill="none">
+                <path d="M25.55 0.999802L10.9501 0.999801" stroke="currentColor"></path>
+                <path d="M25.55 9.3427L10.9501 9.3427" stroke="currentColor"></path>
+                <path d="M25.55 17.6855L10.9501 17.6855" stroke="currentColor"></path>
+                <path d="M25.55 26.0285L10.9501 26.0285" stroke="currentColor"></path>
+                <path d="M25.55 34.3712L10.9501 34.3712" stroke="currentColor"></path>
+                <path d="M25.55 42.714L10.9501 42.714" stroke="currentColor"></path>
+                <path d="M25.55 51.057L10.9501 51.057" stroke="currentColor"></path>
+                <path d="M25.55 59.3997L10.9501 59.3997" stroke="currentColor"></path>
+                <path d="M25.55 67.7425L10.9501 67.7425" stroke="currentColor"></path>
+                <path d="M36.5 76.0854L7.83205e-05 76.0853" stroke="currentColor"></path>
+                <path d="M25.55 84.4284L10.9501 84.4284" stroke="currentColor"></path>
+                <path d="M25.55 92.7713L10.9501 92.7713" stroke="currentColor"></path>
+                <path d="M25.55 101.114L10.9501 101.114" stroke="currentColor"></path>
+                <path d="M25.55 109.457L10.9501 109.457" stroke="currentColor"></path>
+                <path d="M25.55 117.8L10.9501 117.8" stroke="currentColor"></path>
+                <path d="M25.55 126.143L10.9501 126.143" stroke="currentColor"></path>
+                <path d="M25.55 134.485L10.9501 134.485" stroke="currentColor"></path>
+                <path d="M25.55 142.828L10.9501 142.828" stroke="currentColor"></path>
+                <path d="M25.55 151.171L10.9501 151.171" stroke="currentColor"></path>
+            </svg>       
         </div>
         <div className='fixed right-0 top-1/2 -translate-y-1/2'>
             <img className='w-24' src="https://cdn.prod.website-files.com/65ef0d86eff3b9ba6b18aef9/65f302f74ee53940c8b3ece4_border-right-center.svg" alt="" />
         </div>
-
-
-
-
         <div className='absolute items-center flex justify-between px-20 w-full left-0 bottom-20'>
             <img className='h-48 left-0' src="https://cdn.prod.website-files.com/65ef0d86eff3b9ba6b18aef9/6601a44ad564d4d03ce8f94f_Object1.svg" alt="" />
             <img className='h-24 right-0' src="https://cdn.prod.website-files.com/65ef0d86eff3b9ba6b18aef9/6601a486295efb3bb8c5d777_Object2.svg" alt="" />
